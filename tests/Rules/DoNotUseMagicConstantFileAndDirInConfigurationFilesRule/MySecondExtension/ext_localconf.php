2c17a18dad6bbf7777aff400b3c60ae2dab2a8f9
Sebastian Schreiber <me@schreibersebastian.de>
2022-01-13 01:01:58 +0100

    Add rule to check if use statements are used in ext_tables.php or ext_localconf.php

--- tests/Rules/DoNotUseMagicConstantFileAndDirInConfigurationFilesRule/MySecondExtension/ext_localconf.php
+++ tests/Rules/DoNotUseMagicConstantFileAndDirInConfigurationFilesRule/MySecondExtension/ext_localconf.php
@@ -2,5 +2,4 @@
 
 declare(strict_types=1);
 
-
 $file = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('my_second_extension');
---
+++ tests/Rules/DoNotUseUseInConfigurationFilesRule/MyExtension/ext_localconf.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Prefix\MyExtension\PageRendererHooks;
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess']['my_extension'] = PageRendererHooks::class . '->renderPreProcess';
